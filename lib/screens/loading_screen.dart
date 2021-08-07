e29d7537dde8d0efa4e50ce2c7cccf47efeaa824
henryadebayo <henryadebayo250@gmail.com>
2021-08-07 19:34:09 +0100

    branch a

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,21 +1,66 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+const apikey = '961580f6ec754cb00a3060c367618c7b';
+
 
 class LoadingScreen extends StatefulWidget {
+
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  double longitude;
+  double latitude;
+@override
+
+  void initState()async{
+  super.initState();
+  getLocation();
+  }
+
+ void getLocation() async{
+  Location location = Location();
+  await location.getCurrentLocation();
+longitude = location.longitude;
+latitude = location.latitude;
+
+getData();
+
+ }
+ void getData() async{
+
+
+  http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey');
+
+ if (response.statusCode == 200) {
+
+   String data = response.body;
+
+   var decodedData = jsonDecode(data);
+
+   var temp = decodedData["main"]['temp'];
+   var condition =  decodedData['weather'][0]['id'];
+   var countryNme = decodedData['name'];
+
+ }else{
+   print (response.statusCode);
+ }
+}
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+      body: Column(
+        children: [
+         Text('position'),
+          Text('$getLocation().response.statusCode'),
+        ],
       ),
     );
   }
