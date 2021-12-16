6ec9a58a41bdaa2f6714a7447062a398c80a0567
henryadebayo <henryadebayo250@gmail.com>
2021-12-17 00:39:58 +0100

    refactoring

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,65 +1,60 @@
-import 'package:flutter/material.dart';
-import '../services/location.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/location.dart';
+
 const apikey = '961580f6ec754cb00a3060c367618c7b';
 
-
 class LoadingScreen extends StatefulWidget {
-
-
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   double longitude;
   double latitude;
-@override
 
-  void initState()async{
-  super.initState();
-  getLocation();
+  @override
+  initState(){
+    super.initState();
+    getLocation();
   }
 
- void getLocation() async{
-  Location location = Location();
-  await location.getCurrentLocation();
-longitude = location.longitude;
-latitude = location.latitude;
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    longitude = location.longitude;
+    latitude = location.latitude;
 
-getData();
+    getData();
+  }
 
- }
- void getData() async{
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey');
 
+    if (response.statusCode == 200) {
+      String data = response.body;
 
-  http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey');
+      var decodedData = jsonDecode(data);
 
- if (response.statusCode == 200) {
-
-   String data = response.body;
-
-   var decodedData = jsonDecode(data);
-
-   var temp = decodedData["main"]['temp'];
-   var condition =  decodedData['weather'][0]['id'];
-   var countryNme = decodedData['name'];
-
- }else{
-   print (response.statusCode);
- }
-}
+      var temp = decodedData["main"]['temp'];
+      var condition = decodedData['weather'][0]['id'];
+      var countryNme = decodedData['name'];
+    } else {
+      print(response.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Column(
         children: [
-         Text('position'),
-          Text('$getLocation().response.statusCode'),
+          Text('position'),
+          // Text('$getLocation().response.statusCode'),
         ],
       ),
     );
